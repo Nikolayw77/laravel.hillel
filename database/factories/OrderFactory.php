49329e17db48facd31c37fef5d676aa8424a558f
Nikolayw77 <nikolay.w77@gmail.com>
2019-09-10 22:10:38 +0300

    Seeds are working!

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(2),
+        'description' => $faker->sentences(12, true),
+        //'SKU' => FakerPerson::taxId(),
+        'SKU' => $faker->numberBetween(0, 1000000),
+        'price' => $faker->randomFloat(2, 5, 200),
+        'in_stock' => $faker->numberBetween(0, 15),
+        'thumbnail' => $faker->image('public/storage/images/products', 400, 300),
+        'short_description' => $faker->sentences(2, true),
+    ];
+});
